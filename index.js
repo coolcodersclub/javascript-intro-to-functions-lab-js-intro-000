1191e939945c64f8fc1e72ce0d807149720e16e8
Anthony Tortorici <ant.tortorici1@gmail.com>
2018-04-17 22:21:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,15 @@ function logShout(string) {
 function logWhisper(string) {
   return console.log(string.toLowerCase("hello"))
 }
+
+function sayHiToGrandma(string) {
+  if string === lowercase {
+    return "I can\'t hear you!"
+  }
+  if string === uppercase {
+    return "YES INDEED"
+  }
+  if string === "I love you, Grandma." {
+    return "I love you, too."
+  }
+}
